fb6d7dd796a18ae2b9aa69078d97735e92295a77
Kashee3400 <kasheemilkpcl@gmail.com>
2024-06-17 15:46:24 +0530

    Updated Dashboard API View

--- member/urls.py
+++ member/urls.py
@@ -1,4 +1,3 @@
-# urls.py
 from django.urls import path
 from .views import GenerateOTPView, VerifyOTPView,VerifySession,LogoutView
 from erp_app.views import *
@@ -8,8 +7,8 @@ urlpatterns = [
     path('api/otp/generate/', GenerateOTPView.as_view(), name='generate-otp'),
     path('api/otp/verify/', VerifyOTPView.as_view(), name='verify-otp'),
     path('api/verify/session/', VerifySession.as_view(), name='verify-otp'),
-    path('api/members/phone/<str:phone_number>/', MemberByPhoneNumberView.as_view(), name='member-by-phone'),
-    path('api/members/billing-history/<str:member_code>/', BillingMemberDetailView.as_view(), name='member-billing-history'),
+    path('api/members/phone/', MemberByPhoneNumberView.as_view(), name='member-by-phone'),
+    path('api/members/billing-history/', BillingMemberDetailView.as_view(), name='member-billing-history'),
     path('api/mpp-collection/', MppCollectionAggregationListView.as_view(), name='mpp-collection-list'),
     path('api/mpp-collection-detail/', MppCollectionDetailView.as_view(), name='mpp-collection-detail'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
