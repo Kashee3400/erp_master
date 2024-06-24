0213e24d13e500fba789b98691b7b1219bacce90
Kashee3400 <kasheemilkpcl@gmail.com>
2024-06-24 17:48:42 +0530

    models and static otp

--- member/forms.py
+++ member/forms.py
@@ -2,13 +2,15 @@
 
 from django import forms
 from .models import *
-from erp_app.models import Product,Brand,ProductCategory
+from erp_app.models import Product,Brand,ProductCategory, Warehouse, BinLocation
+from django.utils.translation import gettext_lazy as _
+
 
 class ProductImageForm(forms.ModelForm):
     product_code = forms.ModelChoiceField(
         queryset=Product.objects.filter(is_saleable=True),
-        empty_label="Select Product",
-        label="Product"
+        empty_label=_("Select Product"),
+        label=_("Product")
     )
 
     class Meta:
@@ -32,8 +34,8 @@ class ProductImageForm(forms.ModelForm):
 class BrandImageForm(forms.ModelForm):
     brand_code = forms.ModelChoiceField(
         queryset=Brand.objects.all(),
-        empty_label="Select Brand",
-        label="Brand"
+        empty_label=_("Select Brand"),
+        label=_("Brand")
     )
 
     class Meta:
@@ -57,8 +59,8 @@ class BrandImageForm(forms.ModelForm):
 class CategoryImageForm(forms.ModelForm):
     category_code = forms.ModelChoiceField(
         queryset=ProductCategory.objects.all(),
-        empty_label="Select Product Category",
-        label="Product Category"
+        empty_label=_("Select Product Category"),
+        label=_("Product Category")
     )
 
     class Meta:
@@ -77,3 +79,53 @@ class CategoryImageForm(forms.ModelForm):
         if commit:
             instance.save()
         return instance
+
+class ProductStockOpeningForm(forms.ModelForm):
+    product_code = forms.ModelChoiceField(
+        queryset=Product.objects.filter(is_saleable=True),
+        empty_label=_("Select Product"),
+        label=_("Product")
+    )
+    warehouse_code = forms.ModelChoiceField(
+        queryset=Warehouse.objects.all(),
+        empty_label=_("Select Warehouse"),
+        label=_("Warehouse")
+    )
+    bin_location_code = forms.ModelChoiceField(
+        queryset=BinLocation.objects.all(),
+        empty_label=_("Select Bin Location"),
+        label=_("Bin Location")
+    )
+
+    class Meta:
+        model = ProductStockOpening
+        fields = [
+            'product_code', 'warehouse_code', 'bin_location_code',
+            'qty', 'as_on_date', 'created_by',
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['product_code'].label_from_instance = lambda obj: obj.product_name
+        self.fields['warehouse_code'].label_from_instance = lambda obj: obj.warehouse_name
+        self.fields['bin_location_code'].label_from_instance = lambda obj: obj.bin_location_name
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        selected_product = self.cleaned_data['product_code']
+        selected_warehouse = self.cleaned_data['warehouse_code']
+        selected_bin_location = self.cleaned_data['bin_location_code']
+        instance.product_code = selected_product.product_code
+        instance.product_name = selected_product.product_name
+        instance.warehouse_code = int(selected_warehouse.warehouse_code) 
+        instance.warehouse_name = selected_warehouse.warehouse_name
+        instance.bin_location_code = int(selected_bin_location.bin_location_code)  # Ensure the code is an integer
+        instance.bin_location_name = selected_bin_location.bin_location_name
+        
+        if commit:
+            instance.save()
+        return instance
+
+
+class ExcelUploadForm(forms.Form):
+    excel_file = forms.FileField()
--- member/models.py
+++ member/models.py
@@ -94,3 +94,142 @@ class CategoryImage(models.Model):
         verbose_name_plural = _('Category Images')
         app_label = 'member'
 
+
+class ProductStockOpening(models.Model):
+    product_stock_opening_code = models.AutoField(primary_key=True)
+    financial_year_code = models.IntegerField(blank=True, null=True)
+    product_code = models.CharField(max_length=50, verbose_name=_('Product Code'))
+    product_name = models.CharField(max_length=50, verbose_name=_('Product Name'), blank=True, null=True)
+    qty = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
+    as_on_date = models.DateTimeField(blank=True, null=True)
+    warehouse_code = models.CharField(max_length=50,verbose_name=_('Warehouse Code'),blank=True, null=True)
+    warehouse_name = models.CharField(max_length=100,verbose_name=_('Warehouse Name'),blank=True, null=True)
+    bin_location_code = models.CharField(max_length=50,verbose_name=_('Bin Location Code'),blank=True, null=True)
+    bin_location_name = models.CharField(max_length=100,verbose_name=_('Bin Location Name'),blank=True, null=True)
+    created_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,related_name='added_user_stock')
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,related_name='updated_user_stock', blank=True)
+
+    def __str__(self):
+        return f'{self.product_name} {self.qty}'
+    
+    class Meta:
+        db_table = 'product_stock_opening'
+        verbose_name = _('Product Stock Opening')
+        verbose_name_plural = _('Product Stock Opening')
+        app_label = 'member'
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_address',verbose_name=_('User'))
+    street = models.CharField(max_length=100,verbose_name=_('Street', blank=True, null=True))
+    city = models.CharField(max_length=50, verbose_name=_('City'))
+    state = models.CharField(max_length=50, verbose_name=_('State'))
+    country = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.state}, {self.country} - {self.postal_code}"
+    
+class PaymentMethod(models.Model):
+    method = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.method
+    
+    class Meta:
+        db_table = 'tbl_payment_method'
+        verbose_name = 'Payment Method'
+        verbose_name_plural = 'Payment Methods'
+
+
+class Order(models.Model):
+    PENDING = 'Pending'
+    PROCESSED = 'Processed'
+    DELIVERED = 'Delivered'
+    CANCELLED = 'Cancelled'
+
+    ORDER_STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (PROCESSED, 'Processed'),
+        (DELIVERED, 'Delivered'),
+        (CANCELLED, 'Cancelled'),
+    ]
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_orders')
+    status = models.CharField(max_length=20, choices=ORDER_STATUS_CHOICES, default=PENDING)
+    order_comment = models.TextField(blank=True, null=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    order_id = models.CharField(max_length=11, unique=True, editable=False, blank=True,null=True)
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, related_name="order_address")
+    payment_method = models.ForeignKey(PaymentMethod, on_delete=models.SET_NULL, related_name="order_payments", null=True)
+
+    def save(self, *args, **kwargs):
+        self.order_id = self.generate_order_id()
+        super().save(*args, **kwargs)
+
+    def generate_order_id(self):
+        prefix = 'ODR'
+        # Generate random 8-digit number
+        random_digits = ''.join(random.choices(string.digits, k=8))
+        return f'{prefix}{random_digits}'
+
+    def __str__(self):
+        return f"Order for {self.user.username} ({self.status})"
+
+    def mark_as_processed(self):
+        if self.status == self.PENDING:
+            self.status = self.PROCESSED
+            self.save()
+        else:
+            raise ValueError("Cannot mark order as processed. Current status is not Pending.")
+
+    def mark_as_delivered(self):
+        if self.status == self.PROCESSED:
+            self.status = self.DELIVERED
+            self.save()
+        else:
+            raise ValueError("Cannot mark order as delivered. Current status is not Processed.")
+
+    def cancel_order(self):
+        if self.status == self.PENDING or self.status == self.PROCESSED:
+            self.status = self.CANCELLED
+            self.save()
+        else:
+            raise ValueError("Cannot cancel order. Current status is Delivered or Cancelled.")
+        
+    class Meta:
+        db_table = 'tbl_order'
+        managed = True
+        verbose_name = _('Order')
+        verbose_name_plural = _('Orders')
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE, verbose_name='Order')
+    product_code = models.CharField(max_length=50, verbose_name=_('Product Code'))
+    product_name = models.CharField(max_length=50, verbose_name=_('Product Name'), blank=True, null=True)
+    quantity = models.PositiveIntegerField(default=1, verbose_name=_('Quantity'))
+
+    @property
+    def subtotal(self):
+        return self.quantity * self.book.price
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.book.reduce_stock(self.quantity)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.book.title}"
+
+    class Meta:
+        verbose_name = _('Order Item')
+        verbose_name_plural = _('Order Items')
+        
+    class Meta:
+        db_table = 'tbl_order_item'
+        managed = True
+        verbose_name = _('Order Item')
+        verbose_name_plural =_( 'Order Items')
