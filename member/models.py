778340fca06a728bd201085192eabab6ea0434a2
Kashee3400 <kasheemilkpcl@gmail.com>
2024-06-24 14:58:03 +0530

    updated the login credentials

--- member/models.py
+++ member/models.py
@@ -2,9 +2,8 @@ from django.db import models
 import random
 import string
 from django.contrib.auth import get_user_model
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
-from modeltranslation.translator import translator, TranslationOptions
 
 
 User = get_user_model()
@@ -43,4 +42,55 @@ class UserDevice(models.Model):
         db_table = 'user_device'
         verbose_name = _('User Device')
         verbose_name_plural = _('Users\' Devices')
+
+
+class ProductImage(models.Model):
+    product_code = models.CharField(max_length=50, verbose_name=_('Product Code'))
+    product_name = models.CharField(max_length=50, verbose_name=_('Product Name'), blank=True, null=True)
+    image = models.ImageField(upload_to='product_images/')
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name= _('Created At'))
+    updated_at = models.DateTimeField(auto_now=True,verbose_name= _('Updated At'))
+    
+    def __str__(self):
+        return f'{self.product_code}'
+
+    class Meta:
+        db_table = 'product_image'
+        verbose_name = _('Product Image')
+        verbose_name_plural = _('Product Images')
+        app_label = 'member'
+
+
+class BrandImage(models.Model):
+    brand_code = models.CharField(max_length=50, verbose_name=_('Brand Code'))
+    brand_name = models.CharField(max_length=50, verbose_name=_('Brand Name'), blank=True, null=True)
+    image = models.ImageField(upload_to='brand_images/')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created At'))
+    updated_at = models.DateTimeField(auto_now=True,verbose_name= _('Updated At'))
+
+    def __str__(self):
+        return f'{self.brand_code}'
+
+    class Meta:
+        db_table = 'brand_image'
+        verbose_name = _('Brand Image')
+        verbose_name_plural = _('Brand Images')
+        app_label = 'member'
+
+
+class CategoryImage(models.Model):
+    category_code = models.CharField(max_length=50, verbose_name=_('Category Code'))
+    category_name = models.CharField(max_length=50, verbose_name=_('Category Name'), blank=True, null=True)
+    image = models.ImageField(upload_to='category_images/')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created At'))
+    updated_at = models.DateTimeField(auto_now=True,verbose_name= _('Updated At'))
+ 
+    def __str__(self):
+        return f'{self.category_code}'
+
+    class Meta:
+        db_table = 'category_image'
+        verbose_name = _('Category Image')
+        verbose_name_plural = _('Category Images')
+        app_label = 'member'
 
